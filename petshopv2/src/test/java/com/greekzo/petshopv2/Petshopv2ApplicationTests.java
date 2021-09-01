090b5455f485c4d828f4430fece5e76df80fd112
Alonso <thealonso1308@gmail.com>
2021-09-01 17:50:15 -0400

    Adding Spring Boot Application

---
+++ petshopv2/src/test/java/com/greekzo/petshopv2/Petshopv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greekzo.petshopv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petshopv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
